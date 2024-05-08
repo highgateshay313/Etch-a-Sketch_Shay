55d4546360e8417a2df6090106db613dbac2a76a
Legacy <highgateshay@yahoo.com>
2024-05-07 21:28:21 -0500

    Fixed New Grid Event

--- shay.js
+++ shay.js
@@ -7,20 +7,13 @@ const gridBtn = document.querySelector(".btn")
 
 
 gridBtn.addEventListener("click", (e) => {
-    function makeRows(rows, cols) {
-   
-        
-       };
-       let promptValue = Number(prompt("Enter a size for your grid:"));
+    
+        let promptValue = Number(prompt("Enter a size for your grid:"));
        for (i = 0; i < (promptValue * promptValue); i++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-items";
-        makeRows(promptValue);
-       }
-       
-       
-      
-    })
+    };
+ });
     
 
 
